728e13eeb64c162faf27a6d433d2c87e74f95675
Saif Eldeen <zeroident0@gmail.com>
2025-09-03 05:19:44 +0300

    04

--- merto.config.js
+++ merto.config.js
@@ -5,9 +5,9 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.resolveRequest = (context, moduleName, platform) => {
   if (
-    // If the bundle is resolving "event-target-shim" from a module that is part of "react-native-webrtc".
+    // If the bundle is resolving "event-target-shim" from a module that is part of "@stream-io/react-native-webrtc".
     moduleName.startsWith('event-target-shim') &&
-    context.originModulePath.includes('react-native-webrtc')
+    context.originModulePath.includes('@stream-io/react-native-webrtc')
   ) {
     // Resolve event-target-shim relative to the react-native-webrtc package to use v6.
     // React Native requires v5 which is not compatible with react-native-webrtc.
@@ -26,4 +26,14 @@ config.resolver.resolveRequest = (context, moduleName, platform) => {
   return context.resolveRequest(context, moduleName, platform);
 };
 
-module.exports = config;
+module.exports = {
+  resolver: {
+    alias: {
+      // If the bundle is resolving "event-target-shim" from a module that is part of "@stream-io/react-native-webrtc".
+      'event-target-shim': context => 
+        context.originModulePath.includes('@stream-io/react-native-webrtc')
+          ? require.resolve('@stream-io/react-native-webrtc/node_modules/event-target-shim')
+          : require.resolve('event-target-shim')
+    }
+  }
+};
